13ae8b55d856410f4a5414c009f0eaace35d0b16
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2021-06-26 13:16:34 +0530

    init commit

---
+++ Frontend/Routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import  {BrowserRouter,Switch,Route} from "react-router-dom";
+import Home from "./core/Home"
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+
+
+
+const Routes=()=>{
+    return (
+        
+            <BrowserRouter>
+            <Switch>
+                <Route path="/"  exact component={Home}></Route>
+                <Route path="/signup"  exact component={Signup}></Route>
+                <Route path="/Signin"  exact component={Signin}></Route>
+
+            </Switch>
+            
+            </BrowserRouter>
+    )
+}
+
+export default Routes;
